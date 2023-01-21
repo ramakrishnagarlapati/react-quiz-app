9fdeec41b196e9991779a3dcb7a2933eddf53946
Rama krishna <ramakrishnagarlapati04@gmail.com>
2023-01-21 08:46:42 +0000

    Updates

---
+++ precache-manifest.e68d5c83f7d3ca20217fac259cf4ebd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9b4717f2c49a1e93f770acd6533fd20",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "e606e7a56ced2be96672",
+    "url": "/react-quiz-app/static/css/main.8ecc4615.chunk.css"
+  },
+  {
+    "revision": "4a0ea529534f4120b041",
+    "url": "/react-quiz-app/static/js/2.19a06680.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/react-quiz-app/static/js/2.19a06680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e606e7a56ced2be96672",
+    "url": "/react-quiz-app/static/js/main.ee7cbe3d.chunk.js"
+  },
+  {
+    "revision": "8a52b7f32e8880b03d3a",
+    "url": "/react-quiz-app/static/js/runtime-main.c99e15aa.js"
+  }
+]);
